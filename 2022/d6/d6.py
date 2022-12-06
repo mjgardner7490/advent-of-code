ebc857dba1d28e809f644dc008ff49c7630e2d5f
mgardn53 <mgardn53@ford.com>
2022-12-06 06:18:04 -0500

    first solve for d6

--- 2022/d6/d6.py
+++ 2022/d6/d6.py
@@ -1,10 +1,40 @@
 def prob1():
     with open('d6_in.txt','r') as file:
-        pass
+        signal = file.readline()
+        marker = []
+        received = 0
+        for index, char in enumerate(signal):
+            if char not in marker:
+                marker.append(char)
+                if len(marker) == 4:
+                    received = index + 1
+                    break
+            else:
+                dup = marker.index(char)
+                marker = marker[dup+1:] 
+                marker.append(char)
+        print(received)
+
+
+
 
 def prob2():
     with open('d6_in.txt','r') as file:
-        pass
+        signal = file.readline()
+        marker = []
+        received = 0
+        print(signal)
+        for index, char in enumerate(signal):
+            if char not in marker:
+                marker.append(char)
+                if len(marker) == 14:
+                    received = index + 1
+                    break
+            else:
+                dup = marker.index(char)
+                marker = marker[dup+1:] 
+                marker.append(char)
+        print(received)
 
 prob1()
-# prob2()
+prob2()
