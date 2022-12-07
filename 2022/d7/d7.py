af04370a885468e19b0c43f3efe15b15dcd3ea30
mgardn53 <mgardn53@ford.com>
2022-12-07 14:38:07 -0500

    refactored d7

--- 2022/d7/d7.py
+++ 2022/d7/d7.py
@@ -1,8 +1,8 @@
 class Directory(object):
     def __init__(self, data, parent=None):
         self.data = data
+        self.file_size = 0
         self.size = 0
-        self.actual_size = 0
         self.children = []
         self.parent = parent
 
@@ -17,83 +17,82 @@ class Directory(object):
     def is_leaf(self):
         return not self.children
 
-def calc_stuff(tree, smalls):
-    if tree.actual_size != 0:
-        return tree.actual_size
+def calc_dir_size(tree, small_files):
+    if tree.size != 0:
+        return tree.size
 
     if tree.is_leaf():
-        tree.actual_size = tree.size
-        if tree.actual_size <= 100000:
-            smalls.append(tree.actual_size)
-        return tree.actual_size
+        tree.size = tree.file_size
+        if tree.size <= 100000:
+            small_files.append(tree.size)
+        return tree.size
     else:
         size = 0
         for child in tree.children:
-            size += calc_stuff(child, smalls)
+            size += calc_dir_size(child, small_files)
         
-        tree.actual_size = tree.size + size
-        if tree.actual_size <= 100000:
-            smalls.append(tree.actual_size)
-        return tree.actual_size
+        tree.size = tree.file_size + size
+        if tree.size <= 100000:
+            small_files.append(tree.size)
+        return tree.size
     
 
 def prob1():
     with open('d7_in.txt','r') as file:
-        tree = Directory("/")
         file.readline()
+        tree = Directory("/")
         for command in file:
             options = command.split() 
             if options[0] == '$' and options[1] == 'cd':
-                if options[2] != '..':
+                if options[2] == '..':
+                    tree = tree.parent
+                else:
                     for t in tree.children:
                         if t.data == options[2]:
                             tree = t
-                else:
-                    tree = tree.parent
             if options[0] == 'dir':
                 tree.add_child(options[1])
             if options[0].isdigit():
-                tree.size += int(options[0])
+                tree.file_size += int(options[0])
 
-        while !tree.is_root():
+        while not tree.is_root():
             tree = tree.parent
         
-        smalls = []
-        root_size = calc_stuff(tree, smalls)
+        small_files = []
+        root_size = calc_dir_size(tree, small_files)
         
-        print(sum(smalls)) 
+        print(sum(small_files)) 
 
-def calc_stuff2(tree, free, needed):
-    if tree.actual_size >= needed:
-        free.append(tree.actual_size)
+def find_free_space(tree, free, needed):
+    if tree.size >= needed:
+        free.append(tree.size)
     
-    if len(tree.children) >= 0:
+    if not tree.is_leaf():
         for child in tree.children:
-            calc_stuff2(child, free, needed)
+            find_free_space(child, free, needed)
 
 def prob2():
     with open('d7_in.txt','r') as file:
-        tree = Directory("/")
         file.readline()
+        tree = Directory("/")
         for command in file:
             options = command.split() 
             if options[0] == '$' and options[1] == 'cd':
-                if options[2] != '..':
+                if options[2] == '..':
+                    tree = tree.parent
+                else:
                     for t in tree.children:
                         if t.data == options[2]:
                             tree = t
-                else:
-                    tree = tree.parent
             if options[0] == 'dir':
                 tree.add_child(options[1])
             if options[0].isdigit():
-                tree.size += int(options[0])
+                tree.file_size += int(options[0])
 
-        while tree.parent != None:
+        while not tree.is_root():
             tree = tree.parent
         
-        smalls = []
-        root_size = calc_stuff(tree, smalls)
+        root_size = calc_dir_size(tree, [])
         
         avail = 70000000 - root_size
         needed = 30000000 - avail
@@ -101,7 +100,7 @@ def prob2():
         free = []
         free.append(root_size)
         
-        calc_stuff2(tree, free, needed)
+        find_free_space(tree, free, needed)
         
         free.sort()
         print(free[0])
