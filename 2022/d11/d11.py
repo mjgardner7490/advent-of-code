03c88fd55be0b969d987c2755623c480bd0b7f14
mgardn53 <mgardn53@ford.com>
2022-12-10 09:08:14 -0500

    added d11 skeleton

---
+++ 2022/d11/d11.py
@@ -0,0 +1,18 @@
+from pprint import pprint
+
+
+def prob1(): 
+    with open('d11_in_test.txt','r') as file:
+        for instruction in file.readlines():
+            pass
+
+
+def prob2():
+    with open('d11_in_test.txt','r') as file:
+        pass        
+
+prob1()
+# prob2()
+            
+
+
