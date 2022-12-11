1641202988e856799f6aa51000367ca440e74e25
mgardn53 <mgardn53@ford.com>
2022-12-11 12:48:31 -0500

    d12 setup

---
+++ 2022/d12/d12.py
@@ -0,0 +1,13 @@
+def prob1():
+    with open('d12_in_test.txt','r') as file:
+        pass
+
+def prob2(): 
+    with open('d12_in_test.txt','r') as file:
+        pass
+
+prob1()
+# prob2()
+            
+
+
